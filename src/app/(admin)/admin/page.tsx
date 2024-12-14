1d7064f045ced8b982554559ce2157674cf0e645
Elameen Daiyabu <elameendaiyabu@proton.me>
2024-12-14 22:51:26 +0000

    sign up page done

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 export default function AdminPage() {
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10 px-5">
       <h1 className="text-4xl font-bold mb-8">Book Admin</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
